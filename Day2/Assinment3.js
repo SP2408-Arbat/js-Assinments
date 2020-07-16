5275e43cd0a589500ad1c229b8a83333709b9959
SP2408-Arbat <68241020+SP2408-Arbat@users.noreply.github.com>
2020-07-16 12:58:18 +0530

    Add files via upload

---
+++ Day2/Assinment3.js
@@ -0,0 +1,10 @@
+console.log("Helllo")
+let a=String(confirm("Are you 21+ or not"));
+if (a=="true")
+{
+    alert("You can drink");
+}
+else
+{
+    alert("You can not Drink ");
+}
---
+++ Day2/quetion2.js
@@ -0,0 +1,13 @@
+let txt="  Hello my name is Shubham and I am living in Pune  ";
+
+console.log(txt.toUpperCase());
+
+console.log(txt.slice(6, 15));
+
+console.log(txt.substring(2, 9));
+
+//console.log(txt.replaceAll("Hii How are you buddy"));
+
+console.log(txt.replace("Sachin","Shubham"));
+
+console.log(txt.trim())
