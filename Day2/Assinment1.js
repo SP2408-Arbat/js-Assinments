d2260b02507cd860da73baa8de209018befd60bb
SP2408-Arbat <68241020+SP2408-Arbat@users.noreply.github.com>
2020-07-16 12:55:22 +0530

    Day2 Assinment asners

---
+++ Day2/Assinment1.js
@@ -0,0 +1,7 @@
+console.log("Hello");
+let a=Number(prompt("Enter the value of a"));
+let b=Number(prompt("Enter the value of b"));
+alert("Do you want to submit ");
+let result=a+b;
+console.log("Addition is: "+result);
+confirm("Addtion is "+result);
